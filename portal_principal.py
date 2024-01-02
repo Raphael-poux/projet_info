728bc75bf966c09a8c608947281343c92b5f591c
Raphael-poux <144114417+Raphael-poux@users.noreply.github.com>
2024-01-02 11:16:31 +0100

    Update portal_principal.py
    Première version du jeu, sans design

--- portal_principal.py
+++ portal_principal.py
@@ -1,106 +1,147 @@
-import pygame as pg
-from random import randint
-import math
-import random
-
-
-
-x1 = 100 # coordonnée x1 (colonnes) en pixels
-y1 = 100 # coordonnée y1 (lignes) en pixels
-x2 = 400 # coordonnée x2 (colonnes) en pixels
-y2 = 200 # coordonnée y2 (lignes) en pixels
-
-
-
-
-
+import pygame
+import random as rd
 
 
 def main():
-    d=0
-    pg.init()
-    clock = pg.time.Clock()
-    screen = pg.display.set_mode((640, 640))    
-    
-    window_width = 640
-    window_height = 640
-    radius_c = 20    
-    circle_x, circle_y= window_width/2, window_height/2
-    # on initialise pygame et on crée une fenêtre de 640x640 pixels
+    pygame.init()
+    screen = pygame.display.set_mode((720, 720))
+    clock = pygame.time.Clock()
+    White = (255, 255, 255)
+    sol1 = [(i, 410) for i in range(101)]
+    sol2 = [(250 + i, 410) for i in range(471)]
+    sol3 = [(100 + i, 180) for i in range(151)]
+    sols = [sol1, sol2, sol3]
+    plafond1 = [(100 + i, 220) for i in range(151)]
+    plafonds = [plafond1]
+    paroiL1 = [(100, 410 + i) for i in range(311)]
+    paroiL2 = [(250, 180 + i) for i in range(41)]
+    paroisL = [paroiL1, paroiL2]
+    paroiR1 = [(250, 410 + i) for i in range(311)]
+    paroiR2 = [(100, 180 + i) for i in range(41)]
+    paroisR = [paroiR1, paroiR2]
+    surfaces = paroisR + paroisL + plafonds + sols
+    matsurfaces = [[0 for i in range(721)] for j in range(721)]
 
-    # On donne un titre à la fenetre
-    pg.display.set_caption("agario")
+    for sol in sols:
+        for x, y in sol:
+            matsurfaces[y][x] = 1
 
-    # La boucle du jeu
-    done = False
-    while not done:
-        
-        #la boucle fruit:
-        clock.tick(5)
-    
-        ecart_x=pg.mouse.get_pos()[0]-circle_x
-        ecart_y=pg.mouse.get_pos()[1]-circle_y
-        norme=1/6
-        dx=ecart_x*norme
-        dy=ecart_y*norme
-        # Mettez à jour les coordonnées du cercle en ajoutant dx à X et dy à Y
-        
-        circle_x += dx
-        circle_y += dy
-        # on génère une couleur (Rouge, Vert, Bleu) au hasard
-        random_color = (175,20, 125)
-        screen.fill(random_color)
-        blop=pg.draw.circle(screen,color=(255, 0, 0),center=(circle_x, circle_y),radius=radius_c)
-        if d<radius_c:
-            rand_pos=(random.randint(0,640),random.randint(0,640))
-            radius_c+=2
+    for plafond in plafonds:
+        for x, y in plafond:
+            matsurfaces[y][x] = 3
+
+    for paroi in paroisR:
+        for x, y in paroi:
+            matsurfaces[y][x] = 2
+
+    for paroi in paroisL:
+        for x, y in paroi:
+            matsurfaces[y][x] = 4
+
+    def collision(point, surface):
+        for x, y in surface:
+            if (point[0] - x)**2 + (point[1] - y)**2 <= 30**2:
+                return True
+        return False
+
+
+
+    x = 360
+    y = 360
+    vx = 0
+    vy = 0
+    ay = -0.01
+    color = (0, 255, 255)
+
+    en_cours = True
+    portal = {}
+    portals = 0
+    plaf = False
+
+    while en_cours:
+        clock.tick(800)
+        touches = pygame.key.get_pressed()
+        if y > 720:
+            y = 0
+        if x > 720:
+            x = 0
+        if x < 0:
+            x = 720
+        screen.fill((0, 0, 0))
+        pygame.draw.rect(screen, color, (0, 410, 720, 310))
+        pygame.draw.rect(screen, color, (100, 180, 150, 40))
+        pygame.draw.rect(screen, (0, 0, 0), (100, 410, 150, 310))
+        pygame.draw.circle(screen, White, (x, y), 30)
+        dc = [0, 0] 
+        if touches[pygame.K_d]:
+            dc[0] += 1
+        if touches[pygame.K_s]:
+            dc[1] += 1 
+        if touches[pygame.K_q]:
+            dc[0] += -1
+        if touches[pygame.K_z]:
+            dc[1] += -1
+        if dc[0] or dc[1]:
+            xc, yc = x, y
+            while 0 < xc < 720 and 0 < yc < 720 and not matsurfaces[int(yc)][int(xc)]:
+                xc += dc[0]
+                yc += dc[1]
+            pygame.draw.line(screen, (255, 0, 0), (x, y),(xc, yc), 3)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                en_cours = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and (dc[0] or dc[1]) and matsurfaces[int(yc)][int(xc)]:
+                portal[portals] = ((xc, yc), matsurfaces[int(yc)][int(xc)])
+                portals = 1 - portals
+        for key in portal:
+            if portal[key][1] % 2 == 1:
+                rect_ovale = pygame.Rect(portal[key][0][0] - 30, portal[key][0][1] - 7, 60, 14)
+            if portal[key][1] % 2 == 0:
+                rect_ovale = pygame.Rect(portal[key][0][0] - 7, portal[key][0][1] - 30, 14, 60)
+            pygame.draw.ellipse(screen, (255*(1-key), 100*(1-key), 255*key), rect_ovale)
+        if collision((x, y), plafond1) and not plaf:
+            vy = 0
+            plaf = True
+        vy += ay
+        state = sum(collision((x, y), sol) for sol in sols)
+        if state:
+            vy = 0
+            plaf = False
+            vx = 0
+        if touches[pygame.K_RIGHT] and not sum(collision((x, y), paroi) for paroi in paroisR) and vx <= 0.8:
+            vx = 0.8
+        if touches[pygame.K_LEFT] and not sum(collision((x, y), paroi) for paroi in paroisL) and vx >= - 0.8:
+            vx = - 0.8
+        if sum(collision((x, y), paroi) for paroi in paroisL):
+            vx = 0
+            x += 0.3
+        if sum(collision((x, y), paroi) for paroi in paroisR):
+            vx = 0
+            x -= 0.3
+        if touches[pygame.K_UP] and state:
+            vy = 2
+        for key in portal:
+            if (portal[key][0][0] - x)**2 + (portal[key][0][1] - y)**2 <= 32**2 and len(portal) == 2:
+                m = matsurfaces[int(portal[1 - key][0][1])][int(portal[1 - key][0][0])]
+                if  m % 2 == 1:
+                    y = portal[1 - key][0][1] + (m-2)*40
+                    x = portal[1 - key][0][0]
+                    vy = (2-m)*2
+                if m % 2 == 0:
+                    y = portal[1 - key][0][1]
+                    x = portal[1 - key][0][0] + (m-3)*40
+                    vx = (m-3)*1
+        y -= vy
+        x += vx
+        pygame.display.update()
             
-            
-        # Couleur du quadrillage
-        grid_color = (0, 0, 0)  # Noir
-        fruit=pg.draw.circle(screen,color=(0, 0, 255),center=rand_pos,radius=10)
-        
-        # Dimensions du quadrillage
-        num_rows = 20
-        num_cols = 20
-        line_width = 1  # Épaisseur des lignes
-        d=math.sqrt((rand_pos[0]-circle_x)**2+(rand_pos[1]-circle_y)**2)
-
-
-        # Dessinez les lignes horizontales
-        for row in range(num_rows + 1):
-            y = row * window_height // num_rows
-            pg.draw.line(screen, grid_color, (0, y), (window_width, y), line_width)
-
-
-        
-        # Dessinez les lignes verticales
-        for col in range(num_cols + 1):
-            x = col * window_width // num_cols
-            pg.draw.line(screen, grid_color, (x, 0), (x, window_height), line_width)
-            # enfin on met à jour la fenêtre avec tous les changements
-        pg.display.update()
+    pygame.quit()
 
 
 
-
-
-
-
-
-
-        for event in pg.event.get():
-            # chaque évênement à un type qui décrit la nature de l'évênement
-            # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
-            if event.type == pg.QUIT:
-                done = True
-            # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
-            elif event.type == pg.KEYDOWN:
-                # si la touche est "Q" on veut quitter le programme
-                if event.key == pg.K_q:
-                    done = True
-
-    pg.quit()
+# if python says run, then we should run
+if __name__ == "__main__":
+    main()
 
 
 
