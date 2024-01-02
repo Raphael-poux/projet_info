563bafccc01f2bf523df0fbc5e071691137ebcb8
Raphael-poux <144114417+Raphael-poux@users.noreply.github.com>
2024-01-02 12:36:45 +0100

    Update portal_principal.py
    Première Version commentée

--- portal_principal.py
+++ portal_principal.py
@@ -1,125 +1,182 @@
 import pygame
 import random as rd
 
+# Création des différents types de surface sous forme de listes
+
+# On se cogne au sol en descendant
+sol1 = [(i, 410) for i in range(101)]
+sol2 = [(250 + i, 410) for i in range(471)]
+sol3 = [(100 + i, 180) for i in range(151)]
+sols = [sol1, sol2, sol3]
+
+# On se cogne au plafond en montant
+plafond1 = [(100 + i, 220) for i in range(151)]
+plafonds = [plafond1]
+
+# On se cogne aux parois gauches en allant à gauche
+paroiG1 = [(100, 410 + i) for i in range(311)]
+paroiG2 = [(250, 180 + i) for i in range(41)]
+paroisG = [paroiG1, paroiG2]
+
+# On se cogne aux parois droites en allant à droite
+
+paroiD1 = [(250, 410 + i) for i in range(311)]
+paroiD2 = [(100, 180 + i) for i in range(41)]
+paroisD = [paroiD1, paroiD2]
+
+# Les parois sont renseignées dans une matrice avec un chiffre pour chaque type
+# Un niveau correspond à la donnée des surfaces et d'une pos de départ
+matsurfaces = [[0 for i in range(721)] for j in range(721)]
+
+for sol in sols:
+    for j, i in sol:
+        matsurfaces[i][j] = 1
+
+for plafond in plafonds:
+    for j, i in plafond:
+        matsurfaces[i][j] = 3
+
+for paroi in paroisD:
+    for j, i in paroi:
+        matsurfaces[i][j] = 2
+
+for paroi in paroisG:
+    for j, i in paroi:
+        matsurfaces[i][j] = 4
+
+# On définit la fonction collision qui est propre à un disque
+        
+def collision(point, surface):
+    ''' Le fonction prend en entrée une surface et le centre
+    du cercle et indique si le cercle touche la surface'''    
+    for x, y in surface:
+        if (point[0] - x)**2 + (point[1] - y)**2 <= 30**2:
+            return True
+    return False
+
+White = (255, 255, 255)
+Cyan = (0, 255, 255)
+Couleurs_portails = [(255, 100, 0), (0, 0, 255)]
 
 def main():
+    # on initialise pygame et on crée une fenêtre de 720x720 pixels
+    # On donne un titre à la fenetre
     pygame.init()
     screen = pygame.display.set_mode((720, 720))
+    pygame.display.set_caption("Portal")
     clock = pygame.time.Clock()
-    White = (255, 255, 255)
-    sol1 = [(i, 410) for i in range(101)]
-    sol2 = [(250 + i, 410) for i in range(471)]
-    sol3 = [(100 + i, 180) for i in range(151)]
-    sols = [sol1, sol2, sol3]
-    plafond1 = [(100 + i, 220) for i in range(151)]
-    plafonds = [plafond1]
-    paroiL1 = [(100, 410 + i) for i in range(311)]
-    paroiL2 = [(250, 180 + i) for i in range(41)]
-    paroisL = [paroiL1, paroiL2]
-    paroiR1 = [(250, 410 + i) for i in range(311)]
-    paroiR2 = [(100, 180 + i) for i in range(41)]
-    paroisR = [paroiR1, paroiR2]
-    surfaces = paroisR + paroisL + plafonds + sols
-    matsurfaces = [[0 for i in range(721)] for j in range(721)]
 
-    for sol in sols:
-        for x, y in sol:
-            matsurfaces[y][x] = 1
-
-    for plafond in plafonds:
-        for x, y in plafond:
-            matsurfaces[y][x] = 3
-
-    for paroi in paroisR:
-        for x, y in paroi:
-            matsurfaces[y][x] = 2
-
-    for paroi in paroisL:
-        for x, y in paroi:
-            matsurfaces[y][x] = 4
-
-    def collision(point, surface):
-        for x, y in surface:
-            if (point[0] - x)**2 + (point[1] - y)**2 <= 30**2:
-                return True
-        return False
-
-
+    # On initialise la position, la vitesse et l'accélération
 
     x = 360
     y = 360
     vx = 0
     vy = 0
     ay = -0.01
-    color = (0, 255, 255)
-
-    en_cours = True
+    
+    # Les portails sont stockés dans un dictionnaire avec leur couleur
+    # Il contient également le type de surface sur lequel il est posé 
     portal = {}
-    portals = 0
-    plaf = False
+    # On se sert d'un état pour savoir quel portail lancer
+    portalstate = 0
 
-    while en_cours:
+    # On utilise un bool qui devient True quand le cercle touche le plafond
+    # et devient faux quand il touche le sol
+    a_cogne_plafond = False
+
+    # La boucle du jeu
+    done = False
+
+    while not done:
         clock.tick(800)
+
+        # On récupère les touches pressées
         touches = pygame.key.get_pressed()
+
+        # On s'assure que le cercle reste dans la fenêtre, à modifier
         if y > 720:
             y = 0
         if x > 720:
             x = 0
         if x < 0:
             x = 720
+        
+        # Pour actualiser l'écran, on repeint les murs, et le cercle
         screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, color, (0, 410, 720, 310))
-        pygame.draw.rect(screen, color, (100, 180, 150, 40))
+        pygame.draw.rect(screen, Cyan, (0, 410, 720, 310))
+        pygame.draw.rect(screen, Cyan, (100, 180, 150, 40))
         pygame.draw.rect(screen, (0, 0, 0), (100, 410, 150, 310))
         pygame.draw.circle(screen, White, (x, y), 30)
-        dc = [0, 0] 
+
+        # On crée le viseur pour les portails à l'aide de q, s, d, z
+        # On créer un vecteur direction = [abscisse, ordonnée]
+        direction = [0, 0] 
         if touches[pygame.K_d]:
-            dc[0] += 1
+            direction[0] += 1
         if touches[pygame.K_s]:
-            dc[1] += 1 
+            direction[1] += 1 
         if touches[pygame.K_q]:
-            dc[0] += -1
+            direction[0] += -1
         if touches[pygame.K_z]:
-            dc[1] += -1
-        if dc[0] or dc[1]:
-            xc, yc = x, y
-            while 0 < xc < 720 and 0 < yc < 720 and not matsurfaces[int(yc)][int(xc)]:
-                xc += dc[0]
-                yc += dc[1]
+            direction[1] += -1
+
+        # On se sert de la valeur True d'un nombre non nul
+        # On arrête la ligne au premier obstacle ou à la sortie de l'écran
+        curseur = direction[0] or direction[1]
+        if curseur:
+            xc, yc = int(x), int(y)
+            while 0 < xc < 720 and 0 < yc < 720 and not matsurfaces[yc][xc]:
+                xc += direction[0]
+                yc += direction[1]
             pygame.draw.line(screen, (255, 0, 0), (x, y),(xc, yc), 3)
+
+        # On récupère les touches sur lesquelles on a appuyé
+        # On envoie un portail si une surface est disponible
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                en_cours = False
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and (dc[0] or dc[1]) and matsurfaces[int(yc)][int(xc)]:
-                portal[portals] = ((xc, yc), matsurfaces[int(yc)][int(xc)])
-                portals = 1 - portals
-        for key in portal:
-            if portal[key][1] % 2 == 1:
-                rect_ovale = pygame.Rect(portal[key][0][0] - 30, portal[key][0][1] - 7, 60, 14)
-            if portal[key][1] % 2 == 0:
-                rect_ovale = pygame.Rect(portal[key][0][0] - 7, portal[key][0][1] - 30, 14, 60)
-            pygame.draw.ellipse(screen, (255*(1-key), 100*(1-key), 255*key), rect_ovale)
-        if collision((x, y), plafond1) and not plaf:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and curseur and matsurfaces[yc][xc]:
+                    portal[portalstate] = ((xc, yc), matsurfaces[yc][xc], Couleurs_portails[portalstate])
+                    portalstate = 1 - portalstate
+        
+        # Affiche les portails et les oriente selon la surface
+        for portail in portal.values():
+            if portail[1] % 2 == 1:
+                rect_ovale = pygame.Rect(portail[0][0] - 30, portail[0][1] - 7, 60, 14)
+            if portail[1] % 2 == 0:
+                rect_ovale = pygame.Rect(portail[0][0] - 7, portail[0][1] - 30, 14, 60)
+            pygame.draw.ellipse(screen, portail[2], rect_ovale)
+
+        # Annule la vitesse verticale la première fois qu'on touche le plafond
+        if collision((x, y), plafond1) and not a_cogne_plafond:
             vy = 0
-            plaf = True
+            a_cogne_plafond = True
+        
+        # Annule les vitesse verticale et horizontale quand on touche le sol
+        # On sert du fait qu'une somme de booléens vaut True si elle contient True
         vy += ay
-        state = sum(collision((x, y), sol) for sol in sols)
-        if state:
+        touche_sol = sum(collision((x, y), sol) for sol in sols)
+        if touche_sol:
             vy = 0
-            plaf = False
+            a_cogne_plafond = False
             vx = 0
-        if touches[pygame.K_RIGHT] and not sum(collision((x, y), paroi) for paroi in paroisR) and vx <= 0.8:
+
+        # Gère les déplacements latéraux
+        if touches[pygame.K_RIGHT] and not sum(collision((x, y), paroi) for paroi in paroisD) and vx <= 0.8:
             vx = 0.8
-        if touches[pygame.K_LEFT] and not sum(collision((x, y), paroi) for paroi in paroisL) and vx >= - 0.8:
+        if touches[pygame.K_LEFT] and not sum(collision((x, y), paroi) for paroi in paroisG) and vx >= - 0.8:
             vx = - 0.8
-        if sum(collision((x, y), paroi) for paroi in paroisL):
+        if sum(collision((x, y), paroi) for paroi in paroisG):
             vx = 0
             x += 0.3
-        if sum(collision((x, y), paroi) for paroi in paroisR):
+        if sum(collision((x, y), paroi) for paroi in paroisD):
             vx = 0
             x -= 0.3
-        if touches[pygame.K_UP] and state:
+        if touches[pygame.K_UP] and touche_sol:
             vy = 2
+
+        # Fait téléporter le cercle d'un portail à l'autre quand il rentre dedans
         for key in portal:
             if (portal[key][0][0] - x)**2 + (portal[key][0][1] - y)**2 <= 32**2 and len(portal) == 2:
                 m = matsurfaces[int(portal[1 - key][0][1])][int(portal[1 - key][0][0])]
@@ -142,10 +199,4 @@ def main():
 # if python says run, then we should run
 if __name__ == "__main__":
     main()
-
-
-
-
-# if python says run, then we should run
-if __name__ == "__main__":
     main()
